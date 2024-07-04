176c93bd6b00e08181dbf211455c99deb736e823
LIMSEUNGJE <sjlim1217@naver.com>
2024-07-04 17:52:51 +0900

    Merge pull request #6 from 2024-SUMMER-BOOTCAMP-TEAM-A/set/#3
    Design : 공유

---
+++ src/main/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StarField from './StarField';
+
+function App() {
+  return (
+    <div style={{ width: '100%', height: '100vh', margin: 0 }}>
+      <StarField />
+    </div>
+  );
+}
+
+export default App;
