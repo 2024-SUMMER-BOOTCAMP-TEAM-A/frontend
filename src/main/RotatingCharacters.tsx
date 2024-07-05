036a717b03cbcde613f9b782432b6145a1dd6a5f
LIMSEUNGJE <sjlim1217@naver.com>
2024-07-05 22:32:56 +0900

    feat:인격 캐릭터 퍼블리싱
    인결별 캐릭터 모션을 온보딩 3페이지에 추가

--- src/main/RotatingCharacters.tsx
+++ src/main/RotatingCharacters.tsx
@@ -1,9 +1,11 @@
 // 온보딩 3페이지 부분
 import React from 'react';
-import { CharacterContainer, Character1, Character2, Character3, ClickImage } from '../main/mainstyles';
+import { CharacterContainer, Character1, Character2, Character3,Character4,Character5, ClickImage } from '../main/mainstyles';
 import uncleImage from '../assets/uncle.png';
 import luckyImage from '../assets/lucky.png';
 import mzImage from '../assets/mz.png';
+import factImage from '../assets/fact.png';
+import leemalImage from '../assets/leemal.png';
 import clickImg from '../assets/click.png';
 
 const RotatingCharacters: React.FC = () => {
@@ -12,6 +14,8 @@ const RotatingCharacters: React.FC = () => {
       <Character1 src={uncleImage} alt="Character 1" />
       <Character2 src={luckyImage} alt="Character 2" />
       <Character3 src={mzImage} alt="Character 3" />
+      <Character4 src={factImage} alt="Character 4" />
+      <Character5 src={leemalImage} alt="Character 5" />
       <ClickImage src={clickImg} alt="Click" />
     </CharacterContainer>
   );
