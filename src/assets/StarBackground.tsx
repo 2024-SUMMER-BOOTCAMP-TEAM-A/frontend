0b6e069c4d43739422cefe202ba7ca721d231e9e
LIMSEUNGJE <sjlim1217@naver.com>
2024-07-27 20:25:44 +0900

    feat:배경수정

--- src/assets/StarBackground.tsx
+++ src/assets/StarBackground.tsx
@@ -1,4 +1,31 @@
-import React from 'react';
+// import React from 'react';
+// import styled from 'styled-components';
+// import { Stars, Stars1, Stars2 } from '../assets/styles';
+// import ShootingStarsComponent from '../assets/ShootingStarsComponent';
+
+// const StarBackgroundContainer = styled.div`
+//   position: absolute;
+//   width: 100%;
+//   height: 100%;
+//   top: 0;
+//   left: 0;
+//   z-index: 0;
+//   overflow: hidden;
+// `;
+
+// const StarBackground: React.FC = () => {
+//   return (
+//     <StarBackgroundContainer>
+//       <Stars />
+//       <Stars1 />
+//       <Stars2 />
+//       <ShootingStarsComponent />
+//     </StarBackgroundContainer>
+//   );
+// };
+
+// export default StarBackground;
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Stars, Stars1, Stars2 } from '../assets/styles';
 import ShootingStarsComponent from '../assets/ShootingStarsComponent';
@@ -14,8 +41,18 @@ const StarBackgroundContainer = styled.div`
 `;
 
 const StarBackground: React.FC = () => {
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setKey(prevKey => prevKey + 1);  // 키 값을 변경하여 컴포넌트를 리렌더링
+    }, 130000);  // 130초 후 애니메이션을 리셋
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <StarBackgroundContainer>
+    <StarBackgroundContainer key={key}>
       <Stars />
       <Stars1 />
       <Stars2 />
